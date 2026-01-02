bd463a28509acda1b43f26fe9809e50d71641f58
vidhya.m.v <vidhya.m.v@panapps.co>
2026-01-02 18:49:20 +0530

    backend

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import { create, router, defaults } from "json-server";
+import pkg from "json-server";
+import auth from "json-server-auth";
+
+const { create, router, defaults } = pkg;
 
 const app = create();
 const dbRouter = router("data/db.json");
+const middlewares = defaults();
 
-app.use(defaults());
-app.use(express.json());
+app.db = dbRouter.db;
 
-const SECRET = "mysecret";
+app.use(middlewares);
+app.use(auth);
+app.use(dbRouter);
 
-// login
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-
-  const user = dbRouter.db
-    .get("users")
-    .find({ email, password })
-    .value();
-
-  if (!user) return res.status(401).json({ message: "Invalid credentials" });
-
-  const token = jwt.sign({ id: user.id }, SECRET);
-  res.json({ token });
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-// protect rou
